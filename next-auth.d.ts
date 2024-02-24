8d1844eac52f1cc8017b56d914cebdab405fe2ee
Bec4use <netipong.clk@gmail.com>
2024-02-24 21:34:19 +0700

    Finished Project Auth-tutorial

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,6 +3,8 @@ import NextAuth, { type DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
     role: "ADMIN" | "USER" ;
+    isTwoFactorEnabled: boolean;
+    isOAuth: boolean;
 };
 
 declare module "next-auth" {
